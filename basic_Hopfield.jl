35a1c2f6d0ba5965b49f92a105088c0b7c5cdeb7
kai <kai0shimagaki@gmail.com>
2021-06-08 18:32:14 +0200

    I changed something.

--- basic_Hopfield.jl
+++ basic_Hopfield.jl
@@ -72,24 +72,28 @@ end
     OUTPUT: (Xi_p, Xi_m)
 """
 function get_Xi_HP_model(q, L, P_p, P_m, rank_Gamma, evl, evt, f1)
-    Xi_m = zeros(P_m, q*L);
+    Xi_m = zeros(P_m, q*L)
+    scale_m = zeros(P_m)
     for m in 1:P_m
         for i in 1:L
             for a in 1:q
-                Xi_m[m, km(i,a,q)] = sqrt(1.0/evl[end-rank_Gamma+m] - 1) * evt[km(i,a,q), end-rank_Gamma+m] / sqrt(f1[km(i,a,q)]) 
+		    scale_m[m] = sqrt(1.0/evl[end-rank_Gamma+m] - 1)
+		    Xi_m[m, km(i,a,q)] = sqrt(1.0/evl[end-rank_Gamma+m] - 1) * evt[km(i,a,q), end-rank_Gamma+m] / sqrt(f1[km(i,a,q)]) 
             end
         end
     end
     
     Xi_p = zeros(P_p, q*L);
+    scale_p = zeros(P_p)
     for m in 1:P_p
         for i in 1:L
             for a in 1:q
-                Xi_p[m, km(i,a,q)] = sqrt(1- 1.0/evl[end+1-m]) * evt[km(i,a,q),end+1-m] / sqrt(f1[km(i,a,q)]) 
+		    scale_p[m] = sqrt(1- 1.0/evl[end+1-m])
+		    Xi_p[m, km(i,a,q)] = sqrt(1- 1.0/evl[end+1-m]) * evt[km(i,a,q),end+1-m] / sqrt(f1[km(i,a,q)]) 
             end
         end
     end
-    return (Xi_p, Xi_m)
+    return (Xi_p, Xi_m, scale_p, scale_m)
 end
 
 function convert_Xi_HP(Xi_p, Xi_m, P_p, P_m)
@@ -122,7 +126,7 @@ function main_HP(q::Int64, L::Int64, fname_seq::String, file_key::String, fname_
 
 	#P_p = 400; P_m = 400
 	#pseudo_para=0.01
-	(Xi_p, Xi_m)  = get_Xi_HP_model(q, L, P_p, P_m, rank_Gamma, eigenvalues, eigenvectors, f1_original*(1-pseudo_para) +pseudo_para*ones(size(f1_original)));
+	(Xi_p, Xi_m, scale_p, scale_m)  = get_Xi_HP_model(q, L, P_p, P_m, rank_Gamma, eigenvalues, eigenvectors, f1_original*(1-pseudo_para) +pseudo_para*ones(size(f1_original)));
 
 	f_name_out_para = "./para_xi_Pp"*string(P_p)*"_Pm"*string(P_m)*"_"*file_key*".dat"
 	fout = open(f_name_out_para, "w")
@@ -149,7 +153,7 @@ function main_HP(q::Int64, L::Int64, fname_seq::String, file_key::String, fname_
 	p2 = Plots.plot(eigenvalues[ll_index], ll , markershape = :circle, markersize=5, markeralpha=0.5, color="blue", xaxis=:log, label="lgo-likelihood", xlabel="eingenvalues", legend=:topleft, legendfontsize=12)
 	Plots.plot(p1,p2, layout = (2,1), size=(800,700) )
 	Plots.savefig(fname_fig)
-	return (Xi_p, Xi_m, p1, p2)
+	return (Xi_p, Xi_m, scale_p, scale_m, p1, p2)
 end
 
 function main_HP_with_weight(q::Int64, L::Int64, fname_seq::String, fname_w::String, file_key::String, fname_fig::String, P_p::Int64, P_m::Int64, th_reading_MSA=1.1, pseudo_para=0.01)
