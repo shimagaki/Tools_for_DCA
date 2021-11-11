19112db24c08aa07b3bf34b23e0ab6e0c3afa025
Kai Shimagaki <kai0shimagaki@gmail.com>
2021-11-11 15:34:51 -0800

    Duplicate function that can accept a matrix object instead of file name.

--- basic_Hopfield.jl
+++ basic_Hopfield.jl
@@ -156,11 +156,93 @@ function main_HP(q::Int64, L::Int64, fname_seq::String, file_key::String, fname_
 	return (Xi_p, Xi_m, scale_p, scale_m, p1, p2)
 end
 
+function main_HP(q::Int64, L::Int64, X::Array{Int64, 2}, file_key::String, fname_fig::String, P_p::Int64, P_m::Int64, th_reading_MSA=1.1, pseudo_para=0.01)
+	#q,L = 21, 70
+	#fname_seq = "/data/shimagaki/sparse-BM-Analysis/MSA/PF00076_mgap6_compresed_Weff_eq_W.dat"; 
+	#X = get_MSA(fname_seq,q, L, th_reading_MSA);
+	(f1_original,f2_original,c2,Frob) = basic_MSA_analysis(X, q,L);
+	(rank_Gamma, ll, eigenvalues, eigenvectors) = get_eigens_of_Gamma(f2_original, f1_original, q, L, pseudo_para);
+
+	#P_p = 400; P_m = 400
+	#pseudo_para=0.01
+	(Xi_p, Xi_m, scale_p, scale_m)  = get_Xi_HP_model(q, L, P_p, P_m, rank_Gamma, eigenvalues, eigenvectors, f1_original*(1-pseudo_para) +pseudo_para*ones(size(f1_original)));
+
+	f_name_out_para = "./para_xi_Pp"*string(P_p)*"_Pm"*string(P_m)*"_"*file_key*".dat"
+	fout = open(f_name_out_para, "w")
+
+	for m in 1:P_p
+	    for i in 1:L
+		for a in 1:q
+		    println(fout, "Xi_p ", m-1, " ", i-1, " ", a-1, " ", Xi_p[m, km(i,a,q)])
+		end
+	    end
+	end
+	for m in 1:P_m
+	    for i in 1:L
+		for a in 1:q
+		    println(fout, "Xi_m ", m-1, " ", i-1, " ", a-1, " ", Xi_m[m, km(i,a,q)])
+		end
+	    end
+	end
+	close(fout)
+
+	p1 =Plots.plot(eigenvalues, markershape = :circle, markersize=5, markeralpha=0.5, color="blue", xaxis=:log, label="eigenvalues", legend=:topleft, legendfontsize=12)
+
+	ll_index = (q*L-rank_Gamma+1):q*L
+	p2 = Plots.plot(eigenvalues[ll_index], ll , markershape = :circle, markersize=5, markeralpha=0.5, color="blue", xaxis=:log, label="lgo-likelihood", xlabel="eingenvalues", legend=:topleft, legendfontsize=12)
+	Plots.plot(p1,p2, layout = (2,1), size=(800,700) )
+	Plots.savefig(fname_fig)
+	return (Xi_p, Xi_m, scale_p, scale_m, p1, p2)
+end
+
+
 function main_HP_with_weight(q::Int64, L::Int64, fname_seq::String, fname_w::String, file_key::String, fname_fig::String, P_p::Int64, P_m::Int64, th_reading_MSA=1.1, pseudo_para=0.01)
 	#q,L = 21, 70
 	#fname_seq = "/data/shimagaki/sparse-BM-Analysis/MSA/PF00076_mgap6_compresed_Weff_eq_W.dat"; 
 	X = get_MSA_weight(fname_seq, fname_w, q, L, th_reading_MSA);
 	@show size(X)
+	(f1_original,f2_original,c2,Frob) = basic_MSA_analysis(X,w, q,L);
+
+	(rank_Gamma, ll, eigenvalues, eigenvectors) = get_eigens_of_Gamma(f2_original, f1_original, q, L, pseudo_para);
+
+	#P_p = 400; P_m = 400
+	#pseudo_para=0.01
+	(Xi_p, Xi_m)  = get_Xi_HP_model(q, L, P_p, P_m, rank_Gamma, eigenvalues, eigenvectors, f1_original*(1-pseudo_para) +pseudo_para*ones(size(f1_original)));
+
+	f_name_out_para = "./para_xi_Pp"*string(P_p)*"_Pm"*string(P_m)*"_"*file_key*".dat"
+	fout = open(f_name_out_para, "w")
+
+	for m in 1:P_p
+	    for i in 1:L
+		for a in 1:q
+		    println(fout, "Xi_p ", m-1, " ", i-1, " ", a-1, " ", Xi_p[m, km(i,a,q)])
+		end
+	    end
+	end
+	for m in 1:P_m
+	    for i in 1:L
+		for a in 1:q
+		    println(fout, "Xi_m ", m-1, " ", i-1, " ", a-1, " ", Xi_m[m, km(i,a,q)])
+		end
+	    end
+	end
+	close(fout)
+
+	p1 =Plots.plot(eigenvalues, markershape = :circle, markersize=5, markeralpha=0.5, color="blue", xaxis=:log, label="eigenvalues", legend=:topleft, legendfontsize=12)
+
+	ll_index = (q*L-rank_Gamma+1):q*L
+	p2 = Plots.plot(eigenvalues[ll_index], ll , markershape = :circle, markersize=5, markeralpha=0.5, color="blue", xaxis=:log, label="lgo-likelihood", xlabel="eingenvalues", legend=:topleft, legendfontsize=12)
+	Plots.plot(p1,p2, layout = (2,1), size=(800,700) )
+	Plots.savefig(fname_fig)
+	return (Xi_p, Xi_m, p1, p2)
+end
+
+function main_HP_with_weight(q::Int64, L::Int64, X::Array{Int64, 2}, w::Array{Float64, 1}, file_key::String, fname_fig::String, P_p::Int64, P_m::Int64, th_reading_MSA=1.1, pseudo_para=0.01)
+	#q,L = 21, 70
+	#fname_seq = "/data/shimagaki/sparse-BM-Analysis/MSA/PF00076_mgap6_compresed_Weff_eq_W.dat"; 
+	#X = get_MSA_weight(fname_seq, fname_w, q, L, th_reading_MSA);
+	@show size(X), "input should be qL x N dims."
+	@show size(w)
 	(f1_original,f2_original,c2,Frob) = basic_MSA_analysis(X, q,L);
 
 	(rank_Gamma, ll, eigenvalues, eigenvectors) = get_eigens_of_Gamma(f2_original, f1_original, q, L, pseudo_para);
